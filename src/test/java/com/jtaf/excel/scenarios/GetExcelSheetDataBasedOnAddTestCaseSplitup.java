20b63160d0a30a1372d07d473a633e7d5ec5741b
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-02-20 15:10:23 +0530

    Fetch Data Based On Test Scenario Is Updated

--- src/test/java/com/jtaf/excel/scenarios/GetExcelSheetDataBasedOnAddTestCaseSplitup.java
+++ src/test/java/com/jtaf/excel/scenarios/GetExcelSheetDataBasedOnAddTestCaseSplitup.java
@@ -14,11 +14,34 @@ public class GetExcelSheetDataBasedOnAddTestCaseSplitup {
 		for (testCaseRowNum = 1; testCaseRowNum <= rows; testCaseRowNum++) {
 
 			String testCaseName = reader.fetchCellData(sheetName, 0, testCaseRowNum);
-			System.out.println("testCaseName "+testCaseName);
-			System.out.println("testName "+testName);
 			if (testCaseName.equalsIgnoreCase(testName))
 				break;
 		}
 		System.out.println("Test case starts from row num " + testCaseRowNum);
+
+		// Checking total number of test data rows for each test
+		int testDataStartRowNum = testCaseRowNum + 2;
+		int testDataRows = 0;
+		while (!reader.fetchCellData(sheetName, 0, testDataStartRowNum + testDataRows).equalsIgnoreCase("")) {
+			testDataRows++;
+		}
+		System.out.println("Total rows of test data are " + testDataRows);
+
+		// Checking total number of test data columns for each
+		int testDataStartColumNum = testCaseRowNum + 1;
+		int testDataCols = 0;
+		while (!reader.fetchCellData(sheetName, testDataCols, testDataStartColumNum).equalsIgnoreCase("")) {
+			testDataCols++;
+		}
+		System.out.println("Total columns of test data are " + testDataCols);
+
+		// Printing the actual test data of each test
+		for (int rowNum = testDataStartRowNum; rowNum < (testDataStartRowNum + testDataRows); rowNum++) {
+
+			for (int colNum = 0; colNum < testDataCols; colNum++) {
+
+				System.out.println(reader.fetchCellData(sheetName, colNum, rowNum));
+			}
+		}
 	}
 }
